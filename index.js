27938dd3e2045ddc0cdb6fbbee04c7dea5b7c84b
Rafael Carvalho <rafaelcarvalho.rl@gmail.com>
2023-02-20 14:46:08 -0400

    first solution

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-let list = [3, 8, 7, 2, 1]
+let list = [9, 3, 1, 8, 7, 2]
 
 
 function bubbleSort(arr){
-    let i = 0
-    let j = 0
-    for (i; i<arr.length; i++){
+    // let i = 0
+    // let j = 0
+    for (i=0; i<arr.length; i++){
 
-        for(j; j<arr.length; j++){
+        for(j=0; j<arr.length-i-1; j++){
 
-            if(arr[j] < arr[j+1]){
-                var temp = arr[i]
-                arr[i] = arr[j]
-                arr[j] = arr[i]
-                console.log(arr)
+            console.log(i, j, arr[j], arr[j+1])
+            
+            if(arr[j] > arr[j+1]){
+                var temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+            }
+                console.log(list)
             }
 
         // console.log(i, j)
-        }
     }
 }
+
 
 bubbleSort(list)
